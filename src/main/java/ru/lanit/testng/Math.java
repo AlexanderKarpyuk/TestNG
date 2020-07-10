addb77ff7f0319a6efb43f8d15683d98c35e4a41
Alexander <karjuk.sasha@gmail.com>
2020-07-10 16:01:32 +0400

    refactor Math

--- src/main/java/ru/lanit/testng/Math.java
+++ src/main/java/ru/lanit/testng/Math.java
@@ -1,37 +1,24 @@
 package ru.lanit.testng;
 
-import java.text.DecimalFormat;
-
 public class Math {
 
-    public static void main(String[] args) {
-        Math math = new Math();
-        System.out.println(math.divTest(2, 2));
-        Object obj = math.divTest(2, 3);
-        System.out.println((Double)obj + 1);
-    }
-
-    public Object sumTest(double one, double two) {
+    public Object sumTest(Double one, Double two) {
         System.out.println("Складываем " + one + " и " + two);
-        DecimalFormat format = new DecimalFormat("0.#");
-        return format.format(one + two);
+        return one + two;
     }
 
-    public Object subTest(double one, double two) {
+    public Object subTest(Double one, Double two) {
         System.out.println("Вычитаем " + one + " и " + two);
-        DecimalFormat format = new DecimalFormat("0.#");
-        return format.format(one - two);
+        return one - two;
     }
 
-    public Object mulTest(double one, double two) {
+    public Object mulTest(Double one, Double two) {
         System.out.println("Умножаем " + one + " и " + two);
-        DecimalFormat format = new DecimalFormat("0.#");
-        return format.format(one * two);
+        return one * two;
     }
 
-    public Object divTest(double one, double two) {
-        System.out.println("Делим "+ one + " и " + two);
-        DecimalFormat format = new DecimalFormat("0.#");
-        return format.format(one / two);
+    public Object divTest(Double one, Double two) {
+        System.out.println("Делим " + one + " и " + two);
+        return one / two;
     }
 }
