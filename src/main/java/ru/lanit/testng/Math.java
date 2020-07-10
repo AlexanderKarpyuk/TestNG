14854e28af8bc47cb3bd366c85b53d040561be93
Alexander <karjuk.sasha@gmail.com>
2020-07-10 15:16:15 +0400

    implemented calculator

--- src/main/java/ru/lanit/testng/Math.java
+++ src/main/java/ru/lanit/testng/Math.java
@@ -1,10 +1,37 @@
 package ru.lanit.testng;
 
+import java.text.DecimalFormat;
+
 public class Math {
 
-    public Object sumTest(Object two, Object tree) {
-        System.out.println("Складываем "+ two + " и " + tree);
-        int i = (Integer)two + (Integer)tree;
-        return i;
+    public static void main(String[] args) {
+        Math math = new Math();
+        System.out.println(math.divTest(2, 2));
+        Object obj = math.divTest(2, 3);
+        System.out.println((Double)obj + 1);
+    }
+
+    public Object sumTest(double one, double two) {
+        System.out.println("Складываем " + one + " и " + two);
+        DecimalFormat format = new DecimalFormat("0.#");
+        return format.format(one + two);
+    }
+
+    public Object subTest(double one, double two) {
+        System.out.println("Вычитаем " + one + " и " + two);
+        DecimalFormat format = new DecimalFormat("0.#");
+        return format.format(one - two);
+    }
+
+    public Object mulTest(double one, double two) {
+        System.out.println("Умножаем " + one + " и " + two);
+        DecimalFormat format = new DecimalFormat("0.#");
+        return format.format(one * two);
+    }
+
+    public Object divTest(double one, double two) {
+        System.out.println("Делим "+ one + " и " + two);
+        DecimalFormat format = new DecimalFormat("0.#");
+        return format.format(one / two);
     }
 }
