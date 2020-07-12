9a4e8c7c0e71cbeffba2dd61c5ccde4977e84290
Alexander <karjuk.sasha@gmail.com>
2020-07-12 19:50:33 +0400

    Added exceptions and implemented main() from Math

---
+++ src/main/java/ru/lanit/testng/exceptions/IncorrectInputException.java
@@ -0,0 +1,7 @@
+package ru.lanit.testng.exceptions;
+
+public class IncorrectInputException extends Exception {
+    public IncorrectInputException() {
+        super();
+    }
+}
---
+++ src/main/java/ru/lanit/testng/exceptions/OperatorNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.lanit.testng.exceptions;
+
+public class OperatorNotFoundException extends Exception {
+    public OperatorNotFoundException() {
+        super();
+    }
+}
