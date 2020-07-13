d8c3549abc5faa4bb0f32962393f21fde38f8460
Alexander <karjuk.sasha@gmail.com>
2020-07-13 09:49:41 +0400

    refactor JavaDocs in MathTest

--- src/test/java/ru/lanit/framework/steps/MathTest.java
+++ src/test/java/ru/lanit/framework/steps/MathTest.java
@@ -12,6 +12,8 @@ import ru.lanit.testng.Math;
  * Юнит тесты для класса Math.
  */
 public class MathTest extends Assert {
+    String positiveString = "Результат выполнения: %f Ожидаемый результат: %f";
+    String negativeString = "Результат выполнения: %f Результат не должен совпадать с: %f";
     Math math;
 
     @BeforeClass
@@ -23,6 +25,7 @@ public class MathTest extends Assert {
     @AfterClass
     public void tearDown() {
         System.out.println("Подчищаем после тестов");
+        math = null;
     }
 
     /**
@@ -30,25 +33,26 @@ public class MathTest extends Assert {
      * @param one Входной параметр №1 для тестируемого метода.
      * @param two Входной параметр №2 для тестируемого метода.
      * @param three Ожидаемое значение.
+     *              Ожидаемый результат: значения эквивалентны.
      */
     @Test(dataProvider = "positiveSumData", dataProviderClass = TestData.class)
     public void testPositiveSum(double one, double two, double three) {
         double result = math.sumTest(one, two);
         assertEquals(result, three, "Значения не равны");
-        System.out.println(String.format("Результат выполнения: %f Ожидаемый результат: %f", result, three));
+        System.out.println(String.format(positiveString, result, three));
     }
 
     /**
      * Негативный тест для метода сложения - sumTest().
      * @param one Входной параметр №1 для тестируемого метода.
      * @param two Входной параметр №2 для тестируемого метода.
-     *            Ожидаемый результат: выброс исключения.
+     *            Ожидаемый результат: значения НЕ эквивалентны.
      */
     @Test(dataProvider = "negativeData", dataProviderClass = TestData.class)
     public void testNegativeSum(double one, double two, double three) {
         double result = math.sumTest(one, two);
         assertNotEquals(result, three, "Значения равны!");
-        System.out.println(String.format("Результат выполнения: %f Результат не должен совпасть с: %f",
+        System.out.println(String.format(negativeString,
                 result, three));
     }
 
@@ -57,26 +61,26 @@ public class MathTest extends Assert {
      * @param one Входной параметр №1 для тестируемого метода.
      * @param two Входной параметр №2 для тестируемого метода.
      * @param three Ожидаемое значение.
+     *              Ожидаемый результат: значения эквивалентны.
      */
     @Test(dataProvider = "positiveSubData", dataProviderClass = TestData.class)
     public void testPositiveSub(double one, double two, double three) {
         double result = math.subTest(one, two);
         assertEquals(result, three, "Значения не равны");
-        System.out.println(String.format("Результат выполнения: %f Ожидаемый результат: %f", result, three));
+        System.out.println(String.format(positiveString, result, three));
     }
 
     /**
      * Негативный тест для метода вычитания - subTest().
      * @param one Входной параметр №1 для тестируемого метода.
      * @param two Входной параметр №2 для тестируемого метода.
-     *            Ожидаемый результат: выброс исключения.
+     *            Ожидаемый результат: значения НЕ эквивалентны.
      */
     @Test(dataProvider = "negativeData", dataProviderClass = TestData.class)
     public void testNegativeSub(double one, double two, double three) {
         double result = math.subTest(one, two);
         assertNotEquals(result, three, "Значения равны!");
-        System.out.println(String.format("Результат выполнения: %f Результат не должен совпасть с: %f",
-                result, three));
+        System.out.println(String.format(negativeString, result, three));
     }
 
     /**
@@ -84,26 +88,26 @@ public class MathTest extends Assert {
      * @param one Входной параметр №1 для тестируемого метода.
      * @param two Входной параметр №2 для тестируемого метода.
      * @param three Ожидаемое значение.
+     *              Ожидаемый результат: значения эквивалентны.
      */
     @Test(dataProvider = "positiveMulData", dataProviderClass = TestData.class)
     public void testPositiveMul(double one, double two, double three) {
         double result = math.mulTest(one, two);
         assertEquals(result, three, "Значения не равны");
-        System.out.println(String.format("Результат выполнения: %f Ожидаемый результат: %f", result, three));
+        System.out.println(String.format(positiveString, result, three));
     }
 
     /**
      * Негативный тест для метода умножения - mulTest().
      * @param one Входной параметр №1 для тестируемого метода.
      * @param two Входной параметр №2 для тестируемого метода.
-     *            Ожидаемый результат: выброс исключения.
+     *            Ожидаемый результат: значения НЕ эквивалентны.
      */
     @Test(dataProvider = "negativeData", dataProviderClass = TestData.class)
     public void testNegativeMul(double one, double two, double three) {
         double result = math.mulTest(one, two);
         assertNotEquals(result, three, "Значения равны!");
-        System.out.println(String.format("Результат выполнения: %f Результат не должен совпасть с: %f",
-                result, three));
+        System.out.println(String.format(negativeString, result, three));
     }
 
     /**
@@ -111,25 +115,25 @@ public class MathTest extends Assert {
      * @param one Входной параметр №1 для тестируемого метода.
      * @param two Входной параметр №2 для тестируемого метода.
      * @param three Ожидаемое значение.
+     *              Ожидаемый результат: значения эквивалентны.
      */
     @Test(dataProvider = "positiveDivData", dataProviderClass = TestData.class)
     public void testPositiveDiv(double one, double two, double three) {
         double result = math.divTest(one, two);
         assertEquals(result, three, "Значения не равны");
-        System.out.println(String.format("Результат выполнения: %f Ожидаемый результат: %f", result, three));
+        System.out.println(String.format(positiveString, result, three));
     }
 
     /**
      * Негативный тест для метода деления - divTest().
      * @param one Входной параметр №1 для тестируемого метода.
      * @param two Входной параметр №2 для тестируемого метода.
-     *            Ожидаемый результат: выброс исключения.
+     *            Ожидаемый результат: значения НЕ эквивалентны.
      */
     @Test(dataProvider = "negativeData", dataProviderClass = TestData.class)
     public void testNegativeDiv(double one, double two, double three) {
         double result = math.divTest(one, two);
         assertNotEquals(result, three, "Значения равны!");
-        System.out.println(String.format("Результат выполнения: %f Результат не должен совпасть с: %f",
-                result, three));
+        System.out.println(String.format(negativeString, result, three));
     }
 }
